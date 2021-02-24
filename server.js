afbc9ae05adda5907f6717ae398fded12b2ebf70
akinghill <austinkinghill@gmail.com>
2021-02-24 15:40:14 -0700

    docker setup

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ const db = knex({
   client: 'pg',
   connection: {
     host : '127.0.0.1',
-    user : 'austinhill',
+    user : '',
     password : '',
-    database : 'smart-brain'
+    database : 'brain'
   }
 });
 
@@ -31,6 +31,6 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-app.listen(3000, ()=> {
-  console.log('app is running on port 3000');
+app.listen(5000, ()=> {
+  console.log('app is running on port 5000');
 })
